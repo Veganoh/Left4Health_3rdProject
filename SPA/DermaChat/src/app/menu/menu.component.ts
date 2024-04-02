a94ddfc9d9f3434d85ca1eebd2b4826bab1ec3f7
Miguel Silva <mafs687@gmail.com>
2024-04-02 20:54:46 +0100

    Fixed problems with SPA

--- SPA/DermaChat/src/app/menu/menu.component.ts
+++ SPA/DermaChat/src/app/menu/menu.component.ts
@@ -2,14 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
-  standalone: true,
-  imports: [],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
-
 export class MenuComponent {
 
 }
-
-
--- SPA/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { MenuComponent } from './menu/menu.component';
-
-export const routes: Routes = [
-    { path: '', component: MenuComponent}
-];
